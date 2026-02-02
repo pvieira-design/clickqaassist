f7afb2c29cbbc68012be7c29c72f38a03c334871
Pedro Mota <pedromota@MacBook-Pro-de-Pedro.local>
2026-02-02 20:08:08 -0300

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@clickqaassist/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
